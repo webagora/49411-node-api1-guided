a9c32344e8597a582d29269f50f954f58d817450
Ladrillo <gabriel@cabrejas.net>
2022-01-10 20:28:00 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -44,6 +44,9 @@ server.get('/api/dogs/:id', async (req, res) => {
   try {
     const { id } = req.params
     const dog = await Dog.findById(id)
+    if (!dog) {
+      res.status(404).json({ message: 'no dog' })
+    }
     res.status(200).json(dog)
   } catch (err) {
     res.status(500).json({ message: err.message })
