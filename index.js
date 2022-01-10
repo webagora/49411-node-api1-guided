f3571a23fd499c82ff887781e76bd5fb4e263a59
Ladrillo <gabriel@cabrejas.net>
2022-01-10 19:21:57 +0100

    committing to "lecture"

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const server = express()
 
 server.use(express.json()) // parse json from requests
 
+// api is made of endpoints such as
+// http://cats.com/hello_world
 server.get('/hello_world', (req, res) => {
-  
+  res.json('hello world!')
 })
