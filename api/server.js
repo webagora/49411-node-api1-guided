3b5c6455279689b7a361726886a7ddd156b83cc4
Ladrillo <gabriel@cabrejas.net>
2022-01-10 20:15:18 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -36,6 +36,7 @@ server.get('/api/dogs', (req, res) => {
     })
 })
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
+
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
