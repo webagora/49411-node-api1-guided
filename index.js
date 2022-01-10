c4f2db85489124d24c3ca9286873115d8acfed23
Ladrillo <gabriel@cabrejas.net>
2022-01-10 19:43:52 +0100

    committing to "lecture"

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ server.use(express.json()) // parse json from requests
 // api is made of endpoints such as
 // http://cats.com:9000/hello_world
 server.get('/hello_world', (req, res) => {
-  res.json('hello world!!!!!')
+  res.status(201).json('hello world!!!!!')
 })
 
 server.listen(9000, () => {
