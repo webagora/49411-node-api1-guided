8d58633be117efbb6e5090bd08c8352863c0acdf
Ladrillo <gabriel@cabrejas.net>
2022-01-10 20:39:11 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -59,7 +59,11 @@ server.get('/api/dogs/:id', async (req, res) => {
 
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
 server.post('/api/dogs', async (req, res) => {
-  res.json('wohoo, createed doggie!!!')
+  try {
+    const result = await Dog.
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 // [PUT]    /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
