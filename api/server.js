af12d889e8e905d96d90715734b1ec41a415c191
Ladrillo <gabriel@cabrejas.net>
2022-01-10 19:47:29 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -6,10 +6,16 @@ const express = require('express') // commonjs
 const server = express()
 
 // GLOBAL MIDDLEWARE
+server.use(express.json()) // parse json from requests
 
 // ENDPOINTS
 
 // [GET]    /             (Hello World endpoint)
+// api is made of endpoints such as
+// http://cats.com:9000/hello_world
+server.get('/hello_world', (req, res) => {
+  res.status(200).json('hello world!!!!!')
+})
 // [GET]    /api/dogs     (R of CRUD, fetch all dogs)
 // [GET]    /api/dogs/:id (R of CRUD, fetch dog by :id)
 // [POST]   /api/dogs     (C of CRUD, create new dog from JSON payload)
--- index.js
+++ index.js
@@ -3,13 +3,9 @@
 
 
 
-server.use(express.json()) // parse json from requests
 
-// api is made of endpoints such as
-// http://cats.com:9000/hello_world
-server.get('/hello_world', (req, res) => {
-  res.status(200).json('hello world!!!!!')
-})
+
+
 
 server.listen(9000, () => {
   console.log('listening on port 9000')
